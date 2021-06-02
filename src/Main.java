5916b14aaeb0362fb382ddff08fb9c266a768998
Tanishq K <kronorak@outlook.com>
2021-06-02 12:46:35 +0530

    Update Main.java and Main.class
    - Program that showcases the use of a function

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void announceDeveloperTeaTime() {
+        System.out.println("Waiting for developer team time...");
+        System.out.println("Type in random word and press Enter to start developer tea time");
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.next();
+
+        System.out.println();
+
+        System.out.println("It's developer tea time!");
+    }
 
     public static void main(String[] args) {
-
+        announceDeveloperTeaTime();
     }
 
 }
